c934e9fe6985da89994f0f6b4ae68ef04a644ec2
Evgeny Shepelkin <walna75@mail.ru>
2025-05-16 20:41:21 +0300

    Создал доменную модель MetricService

---
+++ HealthDiary/MetricService.Domain/Models/PhysicalActivity.cs
@@ -0,0 +1,23 @@
+﻿namespace MetricService.Domain.Models
+{
+    /// <summary>
+    /// Физическая активность
+    /// </summary>
+    public class PhysicalActivity
+    {
+        /// <summary>
+        /// идентификатор
+        /// </summary>
+        public int Id { get; }
+
+        /// <summary>
+        /// Наименование физической активности
+        /// </summary>
+        public string Name { get; } = string.Empty;
+
+        /// <summary>
+        /// Метаболический эквивалент
+        /// </summary>
+        public float EnergyEquivalent { get; }
+    }
+}
---
+++ HealthDiary/MetricService.Domain/Models/User.cs
@@ -0,0 +1,38 @@
+﻿namespace MetricService.Domain.Models
+{
+    public class User
+    {
+        /// <summary>
+        /// идентификатор
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// дата рождения
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// рост в сантиметрах
+        /// </summary>
+        public Int16 Height { get; set; }
+
+        /// <summary>
+        /// Вес в килограммах
+        /// </summary>
+        public double Weight { get; set; }
+
+        /// <summary>
+        /// возраст
+        /// </summary>
+        /// <returns>Значение возраста (лет)</returns>
+        public int Age
+        {
+            get
+            {
+                return (int)(( DateTime.Now - DateOfBirth).TotalDays / 365.25);
+            }
+        }
+
+    }
+}
