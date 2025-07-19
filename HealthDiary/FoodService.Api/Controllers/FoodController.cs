35c5afe2c40fe38f826421d030e29e60ce2c5e73
constantine17555 <constantine17555@mail.ru>
2025-07-19 18:16:18 +0300

    HEALTHDIARY-35 Добавленн API контроллер

---
+++ HealthDiary/FoodService.Api/Controllers/FoodController.cs
@@ -0,0 +1,33 @@
+﻿using FoodService.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodService.Api.Controllers
+{
+	[ApiController]
+	[Route( "[controller]" )]
+	public class FoodController : ControllerBase
+	{
+		private FoodServiceDbContext _dbContext;
+
+		public FoodController( FoodServiceDbContext dbContext )
+		{
+			_dbContext = dbContext;
+		}
+
+		// to test
+		[HttpGet( nameof( GetProduct ) )]
+		public async Task<IActionResult> GetProduct( int productId )
+		{
+			var product = await _dbContext.Products.FirstOrDefaultAsync( x => x.Id == productId );
+			if ( product != null )
+			{
+				return Ok( product );
+			}
+			else
+			{
+				return NotFound();
+			}
+		}
+	}
+}
