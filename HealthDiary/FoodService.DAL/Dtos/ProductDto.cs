831f2ba776e6ec53acea987fbe9900723a2b1658
constantine17555 <constantine17555@mail.ru>
2025-07-25 09:19:00 +0300

    HEALTHDIARY-79 Доработка модели, добавление AutoMapper.

--- HealthDiary/FoodService.DAL/Dtos/ProductDto.cs
+++ HealthDiary/FoodService.DAL/Dtos/ProductDto.cs
@@ -1,23 +1,18 @@
-﻿namespace Team3.HealthDiary.FoodService.DAL.Entities
+﻿namespace Team3.HealthDiary.FoodService.DAL.Dtos
 {
 	/// <summary>
 	/// Продукт питания
-	/// </summary>
-	/// <param name="Id">Id</param>
+	/// </summary> 
 	/// <param name="Name">Название</param>
 	/// <param name="Calories">Калории на 100г</param>
 	/// <param name="Proteins">Белки на 100г</param>
 	/// <param name="Fats">Жиры на 100г</param>
 	/// <param name="Carbs">Углеводы на 100г</param>
 	/// <param name="InfoSourceType">Источник информации о продукте</param>
-	public record Product(
+	public record ProductDto(
 		string Name,
 		float Calories,
 		float? Proteins,
 		float? Fats,
-		float? Carbs,
-		byte InfoSourceType )
-	{
-		public int Id { get; set; }
-	}
+		float? Carbs );
 }
