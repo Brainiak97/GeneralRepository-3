9f808bc12b31945b96e2d061e830093edf00a038
Evgeny-sch <walna75@mail.ru>
2025-07-31 19:37:41 +0300

    Рефакторинг кода
    Изменить конфигурирование моделей: AnalysisCategory,
    AnalysisResult, AnalysisType
    
    Задача HEALTHDIARY-100

--- HealthDiary/MetricService.Domain/Models/AnalysisResult.cs
+++ HealthDiary/MetricService.Domain/Models/AnalysisResult.cs
@@ -1,14 +1,17 @@
-﻿namespace MetricService.Domain.Models
-{
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace MetricService.Domain.Models
+{
     /// <summary>
     /// Результаты анализов
     /// </summary>
+    [Comment("Результаты анализов")]
     public class AnalysisResult : BaseModel
     {
         /// <summary>
         /// Идентификатор пользователя
         /// </summary>
+        [Comment("Идентификатор пользователя")]
         public int UserId { get; set; }
 
         /// <summary>
@@ -18,34 +21,38 @@
 
         /// <summary>
         /// Идентификатор типа анализа
-        /// </summary>       
+        /// </summary>  
+        [Comment("Тип анализа")]
         public int AnalysisTypeId { get; set; }
 
         /// <summary>
         /// Тип анализа
-        /// </summary>       
+        /// </summary>  
+        [DeleteBehavior(DeleteBehavior.NoAction)]
         public AnalysisType AnalysisType { get; set; } = null!;
-
 
         /// <summary>
         /// Числовое значение результата анализа
-        /// </summary>        
+        /// </summary> 
+        [Comment("Числовое значение результата анализа")]
         public float? Value { get; set; }
-
 
         /// <summary>
         /// Развернутое описание исследования
         /// </summary>
+        [Comment("Развернутое описание исследования")]
         public string? DetailedResearchDescription { get; set; }
 
         /// <summary>
         /// Дата сдачи анализа
-        /// </summary>        
+        /// </summary> 
+        [Comment("Дата сдачи анализа")]
         public DateTime TestedAt { get; set; }
 
         /// <summary>
         /// Любые заметки или замечания по этому анализу
-        /// </summary>        
+        /// </summary>  
+        [Comment("Любые заметки или замечания по этому анализу")]
         public string? Comment { get; set; }
     }
 }
--- HealthDiary/MetricService.Domain/Models/AnalysisType.cs
+++ HealthDiary/MetricService.Domain/Models/AnalysisType.cs
@@ -1,35 +1,47 @@
-﻿namespace MetricService.Domain.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MetricService.Domain.Models
 {
 
     /// <summary>
     /// Типы анализов
     /// </summary>
+    [Comment("Типы анализов")]
     public class AnalysisType : BaseModel 
     {
         /// <summary>
         /// Идентификатор категории анализа
         /// </summary>
+        [Comment("Ссылка на категорию анализа")]
         public int AnalysisCategoryId { get; set; }
 
         /// <summary>
         /// Категория анализа
         /// </summary>
+        [DeleteBehavior(DeleteBehavior.NoAction)]
         public AnalysisCategory AnalysisCategory { get; set; } = null!;
 
         /// <summary>
         /// Название конкретного анализа(например, «Лейкоциты», «Холестерин»)
         /// </summary>
+        [Comment("Название конкретного анализа(например, «Лейкоциты», «Холестерин»)")]
+        [MaxLength(150)]
         public string Name { get; set; } = string.Empty;
 
 
         /// <summary>
         ///Эталонное значение мужской
-        /// </summary>        
+        /// </summary> 
+        [Comment("Эталонное значение мужской")]
+        [MaxLength(150)]
         public string? ReferenceValueMale { get; set; }
 
         /// <summary>
         ///Эталонное значение женский
-        /// </summary>        
+        /// </summary>  
+        [Comment("Эталонное значение женский")]
+        [MaxLength(150)]
         public string? ReferenceValueFemale { get; set; }
 
         
